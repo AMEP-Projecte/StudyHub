f9f20a46bef0992ba0f8977be8b4fe53c8c06e07
1Mari8 <145874794+1Mari8@users.noreply.github.com>
2024-03-23 11:03:30 +0100

    Update TxEditarGrup.cpp

--- TxEditarGrup.cpp
+++ TxEditarGrup.cpp
@@ -4,14 +4,13 @@
 TxEditarGrup::TxEditarGrup(String^ n, String^ t) {
 	_nom = n;
 	_tematica = t;
-	grupExisteix = true;
 }
 
 void TxEditarGrup::executar() {
 	PassarellaGrup^ grup = CercadoraGrup::cercaGrup(_nom);
-	if (grup.obteNom() != " ") {
-		grup.posaTematica(_tematica);
-		grup.modifica();
+	if (grup->obteNom() != " ") {
+		grup->posaTematica(_tematica);
+		grup->modifica();
 	}
-	else grupExisteix = false;
+	else throw gcnew Exception("No existeix aquest grup.");
 }
