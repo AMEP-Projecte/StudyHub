f1df657b818cfa7111277273d12c1aceaf98f4b1
1Mari8 <145874794+1Mari8@users.noreply.github.com>
2024-04-22 12:52:33 +0200

    Update PassarellaPertany.cpp

--- PassarellaPertany.cpp
+++ PassarellaPertany.cpp
@@ -21,3 +21,29 @@ void PassarellaPertany::posaEstudiant(String^ estudiant) {
 void PassarellaPertany::posaGrup(String^ grup) {
 	_grup = grup;
 }
+
+void PassarellaPertany::insereix() {
+	bool totcorrecte = true;
+	String^ connectionString = "Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;";
+	MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
+	String^ sql = "INSERT INTO pertany(estudiant, grup) VALUES('" + _estudiant + "', '" + _grup + "')";
+	MySqlCommand^ cmd = gcnew MySqlCommand(sql, conn);
+	MySqlDataReader^ dataReader;
+	try {
+		// obrim la connexio
+		conn->Open();
+		// executem la comanda (cmd) que s'ha creat abans del try
+		dataReader = cmd->ExecuteReader();
+	}
+	catch (MySqlException^ ex) {
+		MessageBox::Show(ex->Message);
+		totcorrecte = false;
+	}
+	finally {
+		// si tot va be es tanca la connexio
+		if (totcorrecte) {
+			MessageBox::Show("Grup Creat Correctament.");
+		}
+		conn->Close();
+	}
+}
