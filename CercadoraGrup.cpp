3959f8001f1ad39916f9dcfc3eb61cfaa485e8cd
ivanrp14 <ivanramirezpinell@gmail.com>
2024-05-03 15:02:17 +0200

    CercadoraGrup modificada

--- CercadoraGrup.cpp
+++ CercadoraGrup.cpp
@@ -1,9 +1,12 @@
 #include "pch.h"
 #include "CercadoraGrup.h"
+#include "Sistema.h"
+
 
 using namespace MySql::Data::MySqlClient;
 using namespace System;
-
+using namespace System::Windows::Forms;
+using namespace std;
 PassarellaGrup^ CercadoraGrup::cercaPerNomGrup(String^ NomGrup) {
 	String^ connectionString = "Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;";
 	MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
@@ -33,3 +36,56 @@ PassarellaGrup^ CercadoraGrup::cercaPerNomGrup(String^ NomGrup) {
 	conn->Close();
 	return Grup;
 }
+
+List<PassarellaGrup^>^ CercadoraGrup::cercaPerCreador(String^ username)
+{
+
+
+
+	 List<PassarellaGrup^>^ result = gcnew List<PassarellaGrup^>();
+
+    String^ connectionString = "Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;";
+    MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
+
+    String^ sql = String::Format("SELECT * FROM grup WHERE creador = '{0}';", username);
+    MySqlCommand^ cmd = gcnew MySqlCommand(sql, conn);
+    
+
+    MySqlDataReader^ reader = nullptr;
+    Console::WriteLine("PassarellaGrup creada: ");
+    try {
+        // Abrimos la conexión
+        conn->Open();
+
+        // Ejecutamos la consulta
+        reader = cmd->ExecuteReader();
+
+        // Leemos los resultados
+        while (reader->Read()) {
+            String^ nomGrup = safe_cast<String^>(reader["nom"]);
+            String^ tematica = safe_cast<String^>(reader["tematica"]);
+            String^ creador = safe_cast<String^>(reader["creador"]);
+            Console::WriteLine("PassarellaGrup creada: " + nomGrup + tematica+creador);
+            // Creamos un nuevo objeto PassarellaPertany y lo agregamos al resultado
+            PassarellaGrup^ passarella = gcnew PassarellaGrup( nomGrup, tematica, creador);
+            result->Add(passarella);
+        }
+       
+    }
+    catch (Exception^ ex) {
+        MessageBox::Show("Error: " + ex->Message);
+    }
+    finally {
+        // Cerramos el lector
+        if (reader != nullptr) {
+            reader->Close();
+        }
+
+        // Cerramos la conexión
+        conn->Close();
+    }
+
+    return result;
+}
+	
+
--- CercadoraGrup.h
+++ CercadoraGrup.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "PassarellaGrup.h"
 
+using namespace System::Collections::Generic;
 using namespace System;
 
 class CercadoraGrup {
 	public:
 		static PassarellaGrup^ cercaPerNomGrup(String^ NomGrup);
+		static List<PassarellaGrup^>^ CercadoraGrup::cercaPerCreador(String^ username);
 };
 
