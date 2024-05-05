42f7a84d181a954b81143050c08eceea254ac813
alelisette <148685436+alelisette@users.noreply.github.com>
2024-05-05 21:09:08 +0200

    metode Insereix() implementat-PassarellaEspai.cpp

--- PassarellaEspai.cpp
+++ PassarellaEspai.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "PassarellaEspai.h"
-PassarellaEspai::PassarellaEspai(){}
+
+using namespace MySql::Data::MySqlClient;
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace std;
+
+
+PassarellaEspai::PassarellaEspai() {}
 PassarellaEspai::PassarellaEspai(String^ adreca, String^ nom, int capacitat, String^ proveidor) {
 	_adreca = adreca;
 	_nom = nom;
@@ -32,4 +39,28 @@ int PassarellaEspai::obteCapacitat() {
 }
 String^ PassarellaEspai::obteProveidor() {
 	return _userProveidor;
+}
+
+void PassarellaEspai::insereix() {
+	bool totperfe = true;
+	String^ connectionString = "Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;";
+	MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
+	String^ sql = String::Format("INSERT INTO espai VALUES ('{0}', '{1}', '{2}', '{3}');", _adreca, _nom, _capacitat, _userProveidor);
+	MySqlCommand^ cmd = gcnew MySqlCommand(sql, conn);
+	MySqlDataReader^ dataReader;
+	try {
+		conn->Open();
+		dataReader = cmd->ExecuteReader();
+	}
+	catch (MySqlException^ ex) {
+		MessageBox::Show(ex->Message);
+		totperfe = false;
+	}
+	finally {
+		if (totperfe) {
+			MessageBox::Show("Nou espai creat perfectament.");
+		}
+		conn->Close();
+	}
+
 }
