ec74aaceef73879a95be04e6399dec3a14bc8701
alelisette <148685436+alelisette@users.noreply.github.com>
2024-05-05 20:55:17 +0200

    ImplementadaCercaAdmin- CercadoraAdmin.cpp

--- CercadoraAdmin.cpp
+++ CercadoraAdmin.cpp
@@ -1,7 +1,27 @@
 #include "pch.h"
 #include "CercadoraAdmin.h"
-PassarellaAdmin^ CercadoraAdmin::cercaAdmin(String^ username){
-	PassarellaAdmin^ pa = gcnew PassarellaAdmin();
-	pa->posaNomUsuari(username);
+
+PassarellaAdmin^ CercadoraAdmin::cercaAdmin(String^ username) {
+	PassarellaAdmin^ pa = nullptr;
+    String^ connectionString = "Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;"; // TODO-> posar variable connectionString global
+    MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
+    String^ sql = "SELECT * FROM administrador WHERE username = '" + username + "'";
+    MySqlCommand^ cmd = gcnew MySqlCommand(sql, conn);
+    MySqlDataReader^ dataReader;
+    try {
+        conn->Open();
+        dataReader = cmd->ExecuteReader();
+        if (dataReader->Read()) {
+            String^ username = dataReader->GetString(0);
+            pa = gcnew PassarellaAdmin(username);
+        }
+    }
+    catch (Exception^ ex) {
+        // Manejamos el error
+    }
+    finally {
+        // Cerramos la conexión
+        conn->Close();
+    }
 	return pa;
 }
