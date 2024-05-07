9e342c22ed1c3f1f428710f5ddf8f25c30899cb6
ivanrp14 <ivanramirezpinell@gmail.com>
2024-05-07 10:50:33 +0200

    PassarellesCercadores

--- CercadoraGrup.h
+++ CercadoraGrup.h
@@ -2,9 +2,13 @@
 #include "PassarellaGrup.h"
 
 using namespace System;
-
+using namespace MySql::Data::MySqlClient;
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace System::Collections::Generic;
 class CercadoraGrup {
-	public:
-		static PassarellaGrup^ cercaPerNomGrup(String^ NomGrup);
+public:
+	static PassarellaGrup^ cercaPerNomGrup(String^ NomGrup);
+	List<PassarellaGrup^>^ cercaPerCreador(String^ NomCreador);
 };
 
--- PassarellaSessio.cpp
+++ PassarellaSessio.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "PassarellaSessio.h"
-
+using namespace MySql::Data::MySqlClient;
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace System::Collections::Generic;
+using namespace std;
 PassarellaSessio::PassarellaSessio(){}
 PassarellaSessio::PassarellaSessio(String^ grup, String^ data, String^ horaInici, String^ horaFi, String^ adreca, int llocs) {
 	_grup = grup;
@@ -48,3 +52,55 @@ String^ PassarellaSessio::obteAdreca() {
 int PassarellaSessio::obteLlocsLliures() {
 	return _llocsLliures;
 }
+
+
+    void PassarellaSessio::insereix()
+    {
+        String^ connectionString = "Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;";
+        MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
+        bool totcorrecte = true;
+
+        String^ sql = "INSERT INTO sessio (grup, data, hora_inici, hora_fi, adreca, llocs_lliures) VALUES (@grup, @data, @horaInici, @horaFi, @adreca, @llocsLliures)";
+        MySqlCommand^ cmd = gcnew MySqlCommand(sql, conn);
+       
+        // Agregar parámetros
+        cmd->Parameters->AddWithValue("@grup", _grup);
+        cmd->Parameters->AddWithValue("@data", _data);
+        cmd->Parameters->AddWithValue("@horaInici", _horaInici);
+        cmd->Parameters->AddWithValue("@horaFi", _horaFi);
+        cmd->Parameters->AddWithValue("@adreca", _adreca);
+        cmd->Parameters->AddWithValue("@llocsLliures", _llocsLliures);
+
+        try {
+            // Comprobar si la hora de inicio es menor que la hora de fin
+            if (TimeSpan::Parse(_horaInici) >= TimeSpan::Parse(_horaFi)) {
+                throw gcnew Exception("Error de check");
+            }
+
+            conn->Open();
+            cmd->ExecuteNonQuery();
+            MessageBox::Show("Sesión creada correctamente.");
+        }
+        catch (MySqlException^ ex) {
+            if (ex->Number == 1062) {
+                MessageBox::Show("Ya hay una sesion creada para este grupo en esta fecha.");
+            }
+            else {
+                MessageBox::Show(ex->Message);
+            }
+            totcorrecte = false;
+        }
+        catch (Exception^ ex) {
+            if (ex->Message->Contains("check") ){
+                MessageBox::Show("Error de check: La hora de inicio debe ser menor que la hora de fin.");
+            }
+            else {
+                MessageBox::Show(ex->Message);
+            }
+            totcorrecte = false;
+        }
+        finally {
+            conn->Close();
+        }
+    }
+
