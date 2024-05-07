afa0f28d3306158f6095db3400a9dfb96612f984
llukins <lluc.coll.ubia@gmail.com>
2024-05-07 11:57:33 +0200

    Tasks 127, 128, 129, 130, 131

---
+++ EsborrarProveidorUI.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "EsborrarProveidorUI.h"
+
+using namespace StudyHub;
+
+System::Void EsborrarProveidorUI::ButtonTornar_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
