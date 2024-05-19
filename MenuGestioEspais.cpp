8183b3f77f7a5922dd083a62ae9c8f3d0895910b
yingxin chen <yingxin.chen@estudiantat.upc.edu>
2024-05-19 23:58:11 +0200

    gestio espais millores

--- MenuGestioEspais.cpp
+++ MenuGestioEspais.cpp
@@ -31,3 +31,139 @@ System::Void MenuGestioEspais::botoneliminar_Click(System::Object^ sender, Syste
 	Menu->AbrirFormularioEnPanel(Crear);
 }
 
+System::Void MenuGestioEspais::MenuGestioEspais_Load(System::Object^ sender, System::EventArgs^ e){
+
+        // Establecer la conexión a la base de datos MySQL.
+        MySqlConnection^ cn = gcnew MySqlConnection("Server=ubiwan.epsevg.upc.edu; Port=3306; Database=amep04; Uid=amep04; Pwd=aefohC3Johch-;");
+        MySqlCommand^ cmd = gcnew MySqlCommand();
+        MySqlDataAdapter^ da = gcnew MySqlDataAdapter();
+        DataTable^ dt = gcnew DataTable();
+
+        try {
+            cn->Open();
+
+            // Obtener el nombre de usuario del estudiante actual.
+            Sistema^ sist = Sistema::getInstance();
+            String^ username = sist->obteProveidor()->obteNomUsuari();
+
+            String^ sql = "SELECT nom, adreca, capacitat FROM espai WHERE proveidor = @username;";
+           
+            cmd->Connection = cn;
+            cmd->CommandText = sql;
+            cmd->Parameters->AddWithValue("@username", username);
+            da->SelectCommand = cmd;
+
+            da->Fill(dt);
+
+            int rowsCount = dt->Rows->Count;
+            if (rowsCount == 0) {
+                // Mostrar mensaje si no hay sesiones.
+                Label^ noEspais = gcnew Label();
+                noEspais->AutoSize = true;
+                noEspais->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                noEspais->Dock = System::Windows::Forms::DockStyle::Fill;
+                noEspais->Text = L"Encara tens cap espai";
+                noEspais->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                this->tableLayoutPanel1->Controls->Add(noEspais, 0, 1);
+            }
+            else {
+                // Crear y configurar el Panel con AutoScroll habilitado.
+                Panel^ scrollPanel = gcnew Panel();
+                scrollPanel->Dock = System::Windows::Forms::DockStyle::Fill;
+                scrollPanel->AutoScroll = true;
+
+                // Crear y configurar el TableLayoutPanel para mostrar los datos.
+                TableLayoutPanel^ layoutDades = gcnew TableLayoutPanel();
+                layoutDades->AutoSize = true;
+                layoutDades->CellBorderStyle = System::Windows::Forms::TableLayoutPanelCellBorderStyle::Inset;
+                layoutDades->ColumnCount = 3;
+                layoutDades->ColumnStyles->Add(gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 33.33F));
+                layoutDades->ColumnStyles->Add(gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 33.33F));
+                layoutDades->ColumnStyles->Add(gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 33.33F));
+                layoutDades->ForeColor = System::Drawing::Color::White;
+                layoutDades->Dock = System::Windows::Forms::DockStyle::Top; // Change this line to ensure proper scroll behavior
+                layoutDades->RowCount = rowsCount + 1;
+                layoutDades->RowStyles->Add(gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 57));
+                for (int i = 0; i < rowsCount; ++i) {
+                    layoutDades->RowStyles->Add(gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::AutoSize));
+                }
+                layoutDades->TabIndex = 8;
+
+                // Crear y configurar las etiquetas de las columnas.
+                Label^ columnaEspai = gcnew Label();
+                columnaEspai->AutoSize = true;
+                columnaEspai->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                columnaEspai->Dock = System::Windows::Forms::DockStyle::Fill;
+                columnaEspai->Text = L"Espai";
+                columnaEspai->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                Label^ columnaAdreca = gcnew Label();
+                columnaAdreca->AutoSize = true;
+                columnaAdreca->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                columnaAdreca->Dock = System::Windows::Forms::DockStyle::Fill;
+                columnaAdreca->Text = L"Adreca";
+                columnaAdreca->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                Label^ columnaCapacitat = gcnew Label();
+                columnaCapacitat->AutoSize = true;
+                columnaCapacitat->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                columnaCapacitat->Dock = System::Windows::Forms::DockStyle::Fill;
+                columnaCapacitat->Text = L"Capacitat";
+                columnaCapacitat->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                layoutDades->Controls->Add(columnaEspai, 0, 0);
+                layoutDades->Controls->Add(columnaAdreca, 1, 0);
+                layoutDades->Controls->Add(columnaCapacitat, 2, 0);
+
+                // Añadir filas de datos al TableLayoutPanel.
+                for (int i = 0; i < rowsCount; ++i) {
+                    DataRow^ fila = dt->Rows[i];
+
+                    String^ espai = fila["nom"]->ToString();
+                    String^ adrecaSessio = fila["adreca"]->ToString();
+                    String^ capacitatSessio = fila["capacitat"]->ToString();
+
+                    Label^ labelEspai = gcnew Label();
+                    labelEspai->AutoSize = true;
+                    labelEspai->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                    labelEspai->Dock = System::Windows::Forms::DockStyle::Fill;
+                    labelEspai->Text = espai;
+                    labelEspai->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                    Label^ labelAdreca = gcnew Label();
+                    labelAdreca->AutoSize = true;
+                    labelAdreca->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                    labelAdreca->Dock = System::Windows::Forms::DockStyle::Fill;
+                    labelAdreca->Text = adrecaSessio;
+                    labelAdreca->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                    Label^ labelCapacitat = gcnew Label();
+                    labelCapacitat->AutoSize = true;
+                    labelCapacitat->Font = gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0));
+                    labelCapacitat->Dock = System::Windows::Forms::DockStyle::Fill;
+                    labelCapacitat->Text = capacitatSessio;
+                    labelCapacitat->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+
+                    layoutDades->Controls->Add(labelEspai, 0, i + 1);
+                    layoutDades->Controls->Add(labelAdreca, 1, i + 1);
+                    layoutDades->Controls->Add(labelCapacitat, 2, i + 1);
+                }
+
+                // Añadir el TableLayoutPanel al Panel con AutoScroll.
+                scrollPanel->Controls->Add(layoutDades);
+                // Añadir el Panel con AutoScroll al TableLayoutPanel principal.
+                this->tableLayoutPanel1->Controls->Add(scrollPanel, 0, 1);
+            }
+        }
+        catch (MySqlException^ ex) {
+            MessageBox::Show("Error de conexión a la base de datos: " + ex->Message, "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+        }
+        finally {
+            if (cn->State == ConnectionState::Open) {
+                cn->Close();
+            }
+        }
+}
+
+
--- MenuGestioEspais.h
+++ MenuGestioEspais.h
@@ -41,13 +41,12 @@ namespace StudyHub {
 
 
 	private: System::Windows::Forms::Button^ botoneliminar;
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	private: System::Windows::Forms::Label^ label2;
 
 
 	protected:
 
-	protected:
-
-	protected:
 
 	private:
 		/// <summary>
@@ -67,6 +66,9 @@ namespace StudyHub {
 			this->botoneditar = (gcnew System::Windows::Forms::Button());
 			this->botonconsultar = (gcnew System::Windows::Forms::Button());
 			this->botoneliminar = (gcnew System::Windows::Forms::Button());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->tableLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -75,9 +77,9 @@ namespace StudyHub {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 24, System::Drawing::FontStyle::Bold));
 			this->label1->ForeColor = System::Drawing::SystemColors::HighlightText;
-			this->label1->Location = System::Drawing::Point(238, 96);
+			this->label1->Location = System::Drawing::Point(238, 37);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(354, 52);
+			this->label1->Size = System::Drawing::Size(424, 64);
 			this->label1->TabIndex = 2;
 			this->label1->Text = L"Gestionar Espais";
 			this->label1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
@@ -90,7 +92,7 @@ namespace StudyHub {
 				static_cast<System::Byte>(0)));
 			this->botoncrear->ForeColor = System::Drawing::Color::DarkCyan;
 			this->botoncrear->ImageAlign = System::Drawing::ContentAlignment::BottomCenter;
-			this->botoncrear->Location = System::Drawing::Point(161, 177);
+			this->botoncrear->Location = System::Drawing::Point(61, 204);
 			this->botoncrear->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->botoncrear->Name = L"botoncrear";
 			this->botoncrear->Size = System::Drawing::Size(175, 60);
@@ -107,7 +109,7 @@ namespace StudyHub {
 				static_cast<System::Byte>(0)));
 			this->botoneditar->ForeColor = System::Drawing::Color::DarkCyan;
 			this->botoneditar->ImageAlign = System::Drawing::ContentAlignment::BottomCenter;
-			this->botoneditar->Location = System::Drawing::Point(458, 177);
+			this->botoneditar->Location = System::Drawing::Point(61, 289);
 			this->botoneditar->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->botoneditar->Name = L"botoneditar";
 			this->botoneditar->Size = System::Drawing::Size(175, 60);
@@ -124,7 +126,7 @@ namespace StudyHub {
 				static_cast<System::Byte>(0)));
 			this->botonconsultar->ForeColor = System::Drawing::Color::DarkCyan;
 			this->botonconsultar->ImageAlign = System::Drawing::ContentAlignment::BottomCenter;
-			this->botonconsultar->Location = System::Drawing::Point(161, 284);
+			this->botonconsultar->Location = System::Drawing::Point(61, 115);
 			this->botonconsultar->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->botonconsultar->Name = L"botonconsultar";
 			this->botonconsultar->Size = System::Drawing::Size(175, 60);
@@ -141,7 +143,7 @@ namespace StudyHub {
 				static_cast<System::Byte>(0)));
 			this->botoneliminar->ForeColor = System::Drawing::Color::DarkCyan;
 			this->botoneliminar->ImageAlign = System::Drawing::ContentAlignment::BottomCenter;
-			this->botoneliminar->Location = System::Drawing::Point(458, 274);
+			this->botoneliminar->Location = System::Drawing::Point(61, 383);
 			this->botoneliminar->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->botoneliminar->Name = L"botoneliminar";
 			this->botoneliminar->Size = System::Drawing::Size(175, 60);
@@ -150,21 +152,54 @@ namespace StudyHub {
 			this->botoneliminar->UseVisualStyleBackColor = true;
 			this->botoneliminar->Click += gcnew System::EventHandler(this, &MenuGestioEspais::botoneliminar_Click);
 			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->CellBorderStyle = System::Windows::Forms::TableLayoutPanelCellBorderStyle::Single;
+			this->tableLayoutPanel1->ColumnCount = 1;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->Controls->Add(this->label2, 0, 0);
+			this->tableLayoutPanel1->ForeColor = System::Drawing::Color::White;
+			this->tableLayoutPanel1->Location = System::Drawing::Point(294, 114);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 2;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 51)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 100)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(576, 329);
+			this->tableLayoutPanel1->TabIndex = 8;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(4, 1);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(568, 51);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Els meus espais";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
 			// MenuGestioEspais
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 17);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->ClientSize = System::Drawing::Size(809, 455);
+			this->ClientSize = System::Drawing::Size(910, 512);
+			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->botoneliminar);
 			this->Controls->Add(this->botonconsultar);
 			this->Controls->Add(this->botoneditar);
 			this->Controls->Add(this->botoncrear);
 			this->Controls->Add(this->label1);
-			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F));
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(5);
 			this->Name = L"MenuGestioEspais";
 			this->Text = L"MenuGestioEspais";
+			this->Load += gcnew System::EventHandler(this, &MenuGestioEspais::MenuGestioEspais_Load);
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->tableLayoutPanel1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -174,5 +209,6 @@ namespace StudyHub {
 	private: System::Void botonconsultar_Click(System::Object^ sender, System::EventArgs^ e);
 	private: System::Void botoneditar_Click(System::Object^ sender, System::EventArgs^ e);
 	private: System::Void botoneliminar_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void MenuGestioEspais_Load(System::Object^ sender, System::EventArgs^ e);
 };
 }
