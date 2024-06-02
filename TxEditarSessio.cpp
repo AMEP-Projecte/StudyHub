3cf0f3957f4f71caa3219df66ebffbedd64e29ad
1Mari8 <maria.del.carmen.rodriguez.rom@estudiantat.upc.edu>
2024-06-03 00:39:47 +0200

    correcció per Editar Sessió

--- TxEditarSessio.cpp
+++ TxEditarSessio.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "TxEditarSessio.h"
+#include "CercadoraGrup.h"
+#include "Sistema.h"
 
 TxEditarSessio::TxEditarSessio(String^ g, String^ d, String^ hi, String^ hf) {
 	grup = g;
@@ -10,12 +12,18 @@ TxEditarSessio::TxEditarSessio(String^ g, String^ d, String^ hi, String^ hf) {
 }
 
 void TxEditarSessio::executar() {
-	PassarellaSessio^ sessio = gcnew PassarellaSessio();
-	sessio->posaGrup(grup);
-	sessio->posaData(data);
-	sessio->posaHoraInici(hora_inici);
-	sessio->posaHoraFi(nova_hora_fi);
-	sessio->modifica();
+	Sistema^ sistema = Sistema::getInstance();
+	PassarellaGrup^ grupSessio = CercadoraGrup::cercaPerNomGrup(grup);
+
+	if (grupSessio->obtecreador() == sistema->obteUsername()) {
+		PassarellaSessio^ sessio = gcnew PassarellaSessio();
+		sessio->posaGrup(grup);
+		sessio->posaData(data);
+		sessio->posaHoraInici(hora_inici);
+		sessio->posaHoraFi(nova_hora_fi);
+		sessio->modifica();
+	}
+	else throw gcnew Exception("No pots modificar una sessió que no has programat.");
 }
 
 void TxEditarSessio::posaNovaHoraFi(String^ nhf) {
