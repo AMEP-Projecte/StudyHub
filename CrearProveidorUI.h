93028f5744c3fe7087b17bf456d8d6593d42775f
ivanrp14 <ivanramirezpinell@gmail.com>
2024-06-03 11:43:20 +0200

    CrearProveidor

--- CrearProveidorUI.h
+++ CrearProveidorUI.h
@@ -101,11 +101,12 @@ namespace StudyHub {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 24, System::Drawing::FontStyle::Bold));
 			this->label4->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->label4->Location = System::Drawing::Point(114, 34);
+			this->label4->Location = System::Drawing::Point(152, 42);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(352, 42);
+			this->label4->Size = System::Drawing::Size(436, 52);
 			this->label4->TabIndex = 3;
-			this->label4->Text = L"Crear Nou Prove\u00efdor";
+			this->label4->Text = L"Crear Nou Proveïdor";
 			this->label4->Click += gcnew System::EventHandler(this, &CrearProveidorUI::label4_Click);
 			// 
 			// label2
@@ -114,18 +115,20 @@ namespace StudyHub {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13));
 			this->label2->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->label2->Location = System::Drawing::Point(85, 121);
+			this->label2->Location = System::Drawing::Point(113, 149);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(47, 22);
+			this->label2->Size = System::Drawing::Size(59, 26);
 			this->label2->TabIndex = 0;
 			this->label2->Text = L"Nom";
 			// 
 			// textBox1
 			// 
 			this->textBox1->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->textBox1->Location = System::Drawing::Point(199, 123);
+			this->textBox1->Location = System::Drawing::Point(265, 151);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(213, 20);
+			this->textBox1->Size = System::Drawing::Size(283, 22);
 			this->textBox1->TabIndex = 1;
 			// 
 			// label3
@@ -134,18 +137,21 @@ namespace StudyHub {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13));
 			this->label3->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->label3->Location = System::Drawing::Point(85, 170);
+			this->label3->Location = System::Drawing::Point(113, 209);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(112, 22);
+			this->label3->Size = System::Drawing::Size(135, 26);
 			this->label3->TabIndex = 0;
 			this->label3->Text = L"Contrasenya";
 			// 
 			// textBox2
 			// 
 			this->textBox2->Anchor = System::Windows::Forms::AnchorStyles::None;
-			this->textBox2->Location = System::Drawing::Point(199, 172);
+			this->textBox2->Location = System::Drawing::Point(265, 212);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(213, 20);
+			this->textBox2->PasswordChar = '*';
+			this->textBox2->Size = System::Drawing::Size(283, 22);
 			this->textBox2->TabIndex = 1;
 			// 
 			// button3
@@ -153,21 +159,22 @@ namespace StudyHub {
 			this->button3->Anchor = System::Windows::Forms::AnchorStyles::None;
 			this->button3->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->button3->ForeColor = System::Drawing::Color::DarkCyan;
-			this->button3->Location = System::Drawing::Point(227, 279);
+			this->button3->Location = System::Drawing::Point(303, 343);
+			this->button3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(144, 39);
+			this->button3->Size = System::Drawing::Size(192, 48);
 			this->button3->TabIndex = 6;
-			this->button3->Text = L"CREAR PROVE\u00cfDOR";
+			this->button3->Text = L"CREAR PROVEÏDOR";
 			this->button3->UseVisualStyleBackColor = true;
 			this->button3->Click += gcnew System::EventHandler(this, &CrearProveidorUI::button3_Click);
 			// 
 			// CrearProveidorUI
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->ClientSize = System::Drawing::Size(607, 370);
+			this->ClientSize = System::Drawing::Size(809, 455);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->textBox2);
@@ -175,6 +182,7 @@ namespace StudyHub {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label4);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"CrearProveidorUI";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"CrearProveidorUI";
